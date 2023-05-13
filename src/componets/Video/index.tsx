4897e3b6ca0fe3c7770709fac738737c80bd9b89
Macumba45 <gonzalolovo@gmail.com>
2023-05-13 11:02:19 +0200

    feat: LF Storage video

--- src/componets/Video/index.tsx
+++ src/componets/Video/index.tsx
@@ -110,7 +110,7 @@ const Video: FC = () => {
       </SpanBoldContainer>
 
       <VideoContainer>
-        <VideoUrl style={{ height: '600px' }} src={video} controls playsInline preload="auto" />
+        <VideoUrl poster="https://i.postimg.cc/nVdRc8Rc/allison.jpg" style={{ height: '600px' }} src={video} controls playsInline />
       </VideoContainer>
     </MainContainer>
   );
