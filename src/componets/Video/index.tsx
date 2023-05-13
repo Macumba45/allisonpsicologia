ed5fc9f04d327cce2ad2de7c117510b98328e51d
Macumba45 <gonzalolovo@gmail.com>
2023-05-13 12:25:39 +0200

    fix: new video uploaded

--- src/componets/Video/index.tsx
+++ src/componets/Video/index.tsx
@@ -110,7 +110,7 @@ const Video: FC = () => {
       </SpanBoldContainer>
 
       <VideoContainer>
-        <VideoUrl poster="https://i.postimg.cc/nVdRc8Rc/allison.jpg" style={{ height: '600px' }} src={video} controls playsInline />
+        <VideoUrl src={'./../../assets/videos/myvideo1.mp4'} preload="none" poster="https://i.postimg.cc/nVdRc8Rc/allison.jpg" style={{ height: '600px' }} controls playsInline />
       </VideoContainer>
     </MainContainer>
   );
